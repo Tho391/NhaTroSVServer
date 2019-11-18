d5d21071288c6e5d5ce5bc3653b93cd69c7772e0
Tho391 <quangthonguyen391@gmail.com>
2019-11-18 13:00:21 +0700

    --amend

--- Models/NhaTro.js
+++ Models/NhaTro.js
@@ -1,19 +1,22 @@
 var db = require('../connection');
 
-var conn=db.getConnection();
+var conn = db.getConnection();
 
-function getAllpost(req, res, next){
-  var query=
-  conn.query('SELECT DISTINCT idNhaTro,TenChuTro,Sdt,TenDuong,TenQuan,TenTP,localX, localY FROM quanlynhatro1.nhatro,quanlynhatro1.quan,quanlynhatro1.thanhpho,quanlynhatro1.duong where nhatro.idQuan=quan.idQuan and nhatro.idThanhPho=thanhpho.idThanhPho and nhatro.idDuong=duong.idDuong;', function(err,rows){
-    if(err){
-      throw err;
-    }else{
-      // console.log(rows);
-      res.status(201).json(rows);
-    }
-  });
+function getAllpost(req, res, next) {
+  var queryString = "SELECT DISTINCT idNhaTro,TenChuTro,Sdt,TenDuong,TenQuan,TenTP,localX, localY, ImageHinh " +
+    "FROM quanlynhatro1.nhatro,quanlynhatro1.quan,quanlynhatro1.thanhpho,quanlynhatro1.duong, quanlynhatro1.image " +
+    "where nhatro.idQuan=quan.idQuan and nhatro.idThanhPho=thanhpho.idThanhPho and nhatro.idDuong=duong.idDuong and nhatro.idImage = image.idImage;";
+  var query =
+    conn.query(queryString, function (err, rows) {
+      if (err) {
+        throw err;
+      } else {
+        // console.log(rows);
+        res.status(201).json(rows);
+      }
+    });
 }
 
-module.exports={
+module.exports = {
   getAllpost: getAllpost
 }
